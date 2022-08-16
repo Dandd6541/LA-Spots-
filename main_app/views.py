8c76c47c8dc75813661194ce5f71d8ced9740974
Dandd6541 <Mordkid4@gmail.com>
2022-08-16 16:31:39 -0700

    url for home and about

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 # Create your views here.
+def about(request):
+  return HttpResponse('<h1>About the Cat Colelctor</h1>')
