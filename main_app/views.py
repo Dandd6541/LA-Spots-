863688a91c8e20d698e17770d02491595486a8d0
Dandd6541 <Mordkid4@gmail.com>
2022-08-21 19:22:01 -0700

    debugged the Hot Spot page

--- main_app/views.py
+++ main_app/views.py
@@ -2,22 +2,7 @@ from django.shortcuts import render
 from .models import Laspot
 
 # Add the Cat class & list and view function below the imports
-class Laspot:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
 
-laspots_index = [
-  Laspot('Lolo', 'tabby', 'foul little demon', 3),
-  Laspot('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Laspot('Raven', 'black tripod', '3 legged cat', 4)
-]
-
-def laspots_index(request):
-  laspots = Laspot.objects.all()
-  return render(request, 'laspots/index.html', { 'laspots': laspots })
   
 # Define the home view
 def home(request):
@@ -28,4 +13,7 @@ def about(request):
 
 # Add new view
 
+def laspots_index(request):
+  laspots = Laspot.objects.all()
+  return render(request, 'laspots/index.html', { 'laspots': laspots })
 
